7305c0c9ab7cf4fc26af530a14a8f736c27e6b91
ptrompeter <patrick.trompeter@gmail.com>
2019-09-16 17:30:27 -0700

    added a basic html framework.  Kind of.  Requiring o.js for Odata requests from API.  Trying to get my Odata request.  So far, no luck.

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const o = require('odata');
+
+//API endpoint: https://data.seattle.gov/resource/wnbq-64tb.json
+//Odata v2 endpoint: https://data.seattle.gov/OData.svc/wnbq-64tb
+//Odata v4 endpoint: https://data.seattle.gov/api/odata/v4/wnbq-64tb
+//relevant naics codes: "722513", "722511"
+const endpoint = "https://data.seattle.gov/OData.svc/wnbq-64tb";
 
 app.get('/', (request, response) => {
-  response.send('Hey this express server is running.')
+  response.send(getRestaurants())
 });
 
 app.listen(port, (err) => {
@@ -13,3 +20,8 @@ app.listen(port, (err) => {
 
   console.log(`server is listening on ${port}`)
 });
+
+async function getRestaurants(){
+  const response = await o(endpoint).get("OData.svc").query({$filter: `naics_code eq "722513"`});
+  return response;
+}
