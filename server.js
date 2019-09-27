dc0a14186f28cf95b7e0ade5771671e98bdf5096
ptrompeter <patrick.trompeter@gmail.com>
2019-09-26 17:17:53 -0700

    Trying to implement a geoencoding API.  So far, no good.

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 dotenv.config();
 const port = process.env.DEV_PORT;
+const geoApi = process.env.GEO_API;
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 //add the router
@@ -87,6 +88,35 @@ async function sodaCall(zipcode=false) {
   return parsedRes;
 }
 
+async function encodeAddress(address){
+
+}
+
+async function proveAPIWorks(){
+  let url = geoApi.replace("SEARCH_STRING", "Empire%20State%20Building")
+  let init = {}
+  let headers = {
+    "async": true,
+    "crossDomain": true,
+    "url": url,
+    "method": "GET"
+  }
+  init.headers = headers
+  try {
+    let response = await fetch(url, init);
+    let output = await response.json();
+    console.log(output);
+  } catch(err){
+    console.log(err);
+  }
+
+
+  // $.ajax(settings).done(function (response) {
+  //   console.log(response);
+  // });
+}
+
+proveAPIWorks();
 /*
 I'm keeping my homemade filter function for now (below), in case I decide to implement
 a single daily call to the seattle API, then filter and serve requests myself.
