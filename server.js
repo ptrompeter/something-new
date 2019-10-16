c240fd0562f13ef9d31ce5733b3262a3bbdbf94a
ptrompeter <patrick.trompeter@gmail.com>
2019-10-16 16:45:10 -0700

    Cleaned up file organization.

--- server.js
+++ server.js
@@ -92,28 +92,43 @@ const Restaurant = mongoose.model('Restaurant', restaurantSchema);
 const Testrestaurant = mongoose.model('Testrestaurant', restaurantSchema);
 //writing model for last update
 const Lastupdate = mongoose.model('Lastupdate', lastupdateSchema);
-//testing insertion
-let sampleDate = new Date("2019-06-01T00:00:00.000");
-// const sampleRest = new Testrestaurant({
-//   business_legal_name:"ABACUS HOSPITALITY LLC",
-//   trade_name:"FRESH TASTE CAFE",
-//   ownership_type:"LLC - Single Member",
-//   naics_code:"722513",
-//   "naics_description":"Limited-Service Restaurants",
-//   license_start_date: sampleDate,
-//   street_address:"700 STEWART ST",
-//   city_state_zip:"SEATTLE",
-//   state:"WA",
-//   zip:"98101",
-//   business_phone:"360-553-3087",
-//   city_account_number:"0008291010752342",
-//   ubi:"603416636"
-// });
-// sampleRest.save(function (err) {
-//   if (err) return handleError(err);
-// });
 
 
+//ROUTES
+
+router.get('/', function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+//Send to DOM a list of recently opened restaurants filtered by zipcode.
+app.post('/', async function(req,res){
+  let zip = false;
+  let zipRestaurants = false;
+  if (req.body.zip) zip = req.body.zip;
+  zipRestaurants = await sodaCall(zip);
+  res.send(zipRestaurants);
+})
+
+//render a main page.
+app.get('/zip', async function(req,res){
+  let zip = false;
+  let zipRestaurants = false;
+  if (req.query.zipbox) zip = req.query.zipbox;
+  zipRestaurants = await sodaCall(zip);
+  res.send(zipRestaurants);
+})
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('I guess there was an error.', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+});
+
+
+//DATABASE FUNCTIONS
 
 //Write a generalized function for db queries
 async function dbQuery(model, query = {}, message = false) {
@@ -178,137 +193,11 @@ async function callSeattle() {
   console.log("db length:", dbReturn.length);
   return dbReturn;
 }
-//calling the function
 
 
-//wrapper function for testing async functions on launch
-async function testWrapper() {
-  await callSeattle()
-  .catch(function(err){
-    console.log("I'm an error log in callSeattle's catch", err);
-  });
-  await encodeAll()
-  .catch(function(err){
-    console.log("I'm an error log in endcodeAll's catch", err);
-  });
-  let query = await dbQuery(Restaurant, {lat: ""});
-  console.log("HOPING FOR ZERO UNENCODED ENTRIES:", query);
-}
-//running function
-testWrapper()
-.catch(function(err){
-  console.log("I'm an error log in testWapper's catch", err);
-});
+//API CALL FUNCTIONS
 
-//Testing on sample data.
-//sample data for batch insertion.
-const sampleDataArray = [
-  {"business_legal_name":"ABACUS HOSPITALITY LLC","trade_name":"FRESH TASTE CAFE","ownership_type":"LLC - Single Member","naics_code":"722513","naics_description":"Limited-Service Restaurants","license_start_date":"2019-06-01T00:00:00.000","street_address":"700 STEWART ST","city_state_zip":"SEATTLE","state":"WA","zip":"98101","business_phone":"360-553-3087","city_account_number":"0008291010752342","ubi":"603416636"},
-  {"business_legal_name":"CRAB POT RESTAURANTS INC","trade_name":"THE CRAB POT","ownership_type":"Corporation","naics_code":"722511","naics_description":"Full-Service Restaurants","license_start_date":"2019-01-01T00:00:00.000","street_address":"1301 ALASKAN WAY","city_state_zip":"SEATTLE","state":"WA","zip":"98101","business_phone":"206-623-8600","city_account_number":"0008308990745210","ubi":"6042822600010001"},
-  {"business_legal_name":"PREMIER MEAT PIES LLC","trade_name":"PREMIER MEAT PIES LLC","ownership_type":"LLC - Multi Member","naics_code":"722513","naics_description":"Limited-Service Restaurants","license_start_date":"2018-11-01T00:00:00.000","street_address":"1001 ALASKAN WAY # 105","city_state_zip":"SEATTLE","state":"WA","zip":"98101","business_phone":"206-619-0499","city_account_number":"0007842320744027","ubi":"603512468"},
-  {"business_legal_name":"SAI RESTAURANTS ENTERPRISE INC","trade_name":"ZAIKA RESTAURANT AND LOUNGE","ownership_type":"Corporation","naics_code":"722511","naics_description":"Full-Service Restaurants","license_start_date":"2019-09-11T00:00:00.000","street_address":"1100 PIKE ST","city_state_zip":"SEATTLE","state":"WA","zip":"98101","business_phone":"206-499-4949","city_account_number":"0007312120755875","ubi":"6030512120010002"},
-  {"business_legal_name":"SCHWARTZ BROTHERS RESTAURANTS","trade_name":"DANIELS BROILER DOWNTOWN SEATTLE","ownership_type":"General Partnership","naics_code":"722511","naics_description":"Full-Service Restaurants","license_start_date":"2018-12-01T00:00:00.000","street_address":"808 HOWELL ST #200","city_state_zip":"SEATTLE","state":"WA","zip":"98101","business_phone":"425-455-3948","city_account_number":"0005724290742676","ubi":"6024090030010016"},
-  {"business_legal_name":"WASHINGTON TAPROOMS LLC","trade_name":"LOCUST CIDER","ownership_type":"LLC - Multi Member","naics_code":"722511","naics_description":"Full-Service Restaurants","license_start_date":"2019-09-12T00:00:00.000","street_address":"1222 POST ALY","city_state_zip":"SEATTLE","state":"WA","zip":"98101","business_phone":"646-783-9267","city_account_number":"0008416520756455","ubi":"6044539810010001"}
-];
-
-//update the 722513 records to be more than one year old
-async function makeSomeOld() {
-  let allRecords = await getSampleOutput(Testrestaurant);
-  console.log("allRecords:", allRecords);
-  async function transform(entries) {
-    entries.forEach(async function(entry) {
-      if (entry.naics_code === '722513'){
-        entry.license_start_date.setFullYear(entry.license_start_date.getFullYear() - 1);
-        await entry.markModified('license_start_date');
-        await entry.save();
-      }
-    })
-    let changeFiles = await getSampleOutput(Testrestaurant, {naics_code: '722513'});
-    return changeFiles;
-    // console.log("MODIFIED FILES:", changeFiles);
-  }
-  await transform(allRecords);
-  let oldRecords = await getSampleOutput(Testrestaurant, {naics_code: '722513'}, "If these have old dates, you win.");
-  // await removeTestRestaurants();
-  return oldRecords;
-}
-
-//Write a basic query that returns the contents of testrestaurants
-async function getSampleOutput(model, query = {}, message = "No message passed") {
-  console.log("message:", message);
-  let rawResponse = await model.find(query, function(err, restaurants) {
-    return restaurants;
-  });
-  console.log("response from getSampleOutput", rawResponse);
-  console.log("Count:", rawResponse.length);
-  return rawResponse;
-};
-
-
-//empty testrestaurants
-async function removeTestRestaurants(){
-  let response = await Testrestaurant.remove({});
-  console.log("Records deleted:", response.deletedCount);
-}
-
-//Composite testing function
-async function runTestCode() {
-  mongoose.connect('mongodb://localhost/test', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-  });
-  await insertRestaurant(sampleDataArray, Testrestaurant);
-  let oldRecords = await makeSomeOld();
-  await removeTestRestaurants();
-  mongoose.connect('mongodb://localhost/restaurants', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-  });
-  return oldRecords;
-}
-
-//run test code
-// runTestCode()
-// .catch(function(err){
-//   console.log("I'm an error log in runTestCode's catch", err);
-// });
-
-
-
-//ROUTES
-router.get('/', function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
-});
-
-//Send to DOM a list of recently opened restaurants filtered by zipcode.
-app.post('/', async function(req,res){
-  let zip = false;
-  let zipRestaurants = false;
-  if (req.body.zip) zip = req.body.zip;
-  zipRestaurants = await sodaCall(zip);
-  res.send(zipRestaurants);
-})
-
-//render a main page.
-app.get('/zip', async function(req,res){
-  let zip = false;
-  let zipRestaurants = false;
-  if (req.query.zipbox) zip = req.query.zipbox;
-  zipRestaurants = await sodaCall(zip);
-  res.send(zipRestaurants);
-})
-
-app.listen(port, (err) => {
-  if (err) {
-    return console.log('I guess there was an error.', err)
-  }
-
-  console.log(`server is listening on ${port}`)
-});
-
+//Query open database for restaurant data
 async function sodaCall(zipcode=false, lastUpdate = false) {
   let newInit = {};
   let headers = {};
@@ -347,7 +236,7 @@ async function sodaCall(zipcode=false, lastUpdate = false) {
   return parsedRes;
 }
 
-//take an instance of the restaurant model, get lat and long from geoencode API
+//take an instance of the restaurant model, get lat and long from geoencoding API
 async function geoEncode(restaurant) {
   console.log("Hit geoEncode");
   let address = restaurant.street_address + ", " + restaurant.city_state_zip + ", " + restaurant.state + ", " + restaurant.zip;
@@ -371,7 +260,8 @@ async function geoEncode(restaurant) {
     console.log("this log an error in geoEncode", err);
   }
 }
-//Function to try to match a search result to an instance
+
+//Match a geoencoding search result to a restaurant instance
 async function parseGeoResponse(instance, geoArray){
   if (geoArray.length == 0) return false;
   let name = instance.trade_name.toLowerCase()
@@ -394,15 +284,12 @@ async function addLatLong(restaurant, singleResult) {
   return restaurant;
 }
 
-//Get all restaurants lacking geoEncoding, encode them, save.
+//Get all restaurants lacking geoEncoding from DB, encode them, save.
 async function encodeAll(){
   let unEncodedRests = await dbQuery(Restaurant, {lat: ""}, "getting unencoded restaurants");
   let counter = 0;
   let restsLength = unEncodedRests.length;
-  // console.log("unEncodedRests", unEncodedRests);
   console.log("unEncodedRests.length:", unEncodedRests.length);
-  console.log("unEncodedRests[0]", unEncodedRests[0]);
-  // console.log("unEcodedRests[5]", unEncodedRests[5]);
     //Attempting to regulate calls via setInterval
     try {
       let promise = new Promise((resolve, reject) => {
@@ -430,51 +317,30 @@ async function encodeAll(){
 
 }
 
-async function testGeoEncode() {
-  let allRestaurants = await dbQuery(Restaurant, {}, "getting all restaurants");
-  console.log("to be encoded:", allRestaurants[0]);
-  let response = await geoEncode(allRestaurants[0]);
-  console.log("Response from API:", response);
-  let singleRes = await parseGeoResponse(allRestaurants[0], response);
-  console.log("Matched response to be added to instance:", singleRes);
-  if (singleRes) {
-    console.log("lat / long to insert:", singleRes.lat, singleRes.lon)
-    allRestaurants[0].lat = singleRes.lat;
-    allRestaurants[0].markModified("lat");
-    allRestaurants[0].long = singleRes.lon;
-    allRestaurants[0].markModified("long");
-    // await allRestaurants[0].markModified("long");
-    await allRestaurants[0].save();
-    console.log("What is allrestaurants[0]?", typeof allRestaurants[0])
-    console.log("Item should now be updated", allRestaurants[0].lat, allRestaurants[0].long);
-    let query = await dbQuery(Restaurant, {trade_name: allRestaurants[0].trade_name});
-    console.log("Final Request from db - should have coords", query);
-    return query;
-  }
+//MANAGE STARTUP FUNCTION EXECUTIONS
+
+//wrapper function for testing async functions on launch
+async function testWrapper() {
+  await callSeattle()
+  .catch(function(err){
+    console.log("I'm an error log in callSeattle's catch", err);
+  });
+  await encodeAll()
+  .catch(function(err){
+    console.log("I'm an error log in endcodeAll's catch", err);
+  });
+  let query = await dbQuery(Restaurant, {lat: ""});
+  console.log("HOPING FOR ZERO UNENCODED ENTRIES:", query);
 }
 
-// testGeoEncode();
+//running function
+testWrapper()
+.catch(function(err){
+  console.log("I'm an error log in testWapper's catch", err);
+});
 
-async function proveAPIWorks(){
-  let url = geoApi.replace("SEARCH_STRING", "Empire%20State%20Building");
-  console.log("url:", url);
-  console.log("this log is in proveAPIWorks - typeof url", typeof url);
-  let init = {};
-  let headers = {
-    "async": true,
-    "crossDomain": true,
-    "url": url,
-    "method": "GET"
-  }
-  init.headers = headers
-  try {
-    let response = await fetch(url, init);
-    let output = await response.json();
-    console.log("this is the output log in proveAPIWorks", output);
-  } catch(err){
-    console.log("this log an error in proveAPIWorks", err);
-  }
-}
+
+//TEST CODE BELOW
 
 /*
 I'm keeping my homemade filter function for now (below), in case I decide to implement
@@ -492,5 +358,121 @@ a single daily call to the seattle API, then filter and serve requests myself.
 //     return timeFilter;
 //   } catch(err){
 //     console.log(err);
+//   }
+// }
+
+//testing insertion
+// let sampleDate = new Date("2019-06-01T00:00:00.000");
+// const sampleRest = new Testrestaurant({
+//   business_legal_name:"ABACUS HOSPITALITY LLC",
+//   trade_name:"FRESH TASTE CAFE",
+//   ownership_type:"LLC - Single Member",
+//   naics_code:"722513",
+//   "naics_description":"Limited-Service Restaurants",
+//   license_start_date: sampleDate,
+//   street_address:"700 STEWART ST",
+//   city_state_zip:"SEATTLE",
+//   state:"WA",
+//   zip:"98101",
+//   business_phone:"360-553-3087",
+//   city_account_number:"0008291010752342",
+//   ubi:"603416636"
+// });
+// sampleRest.save(function (err) {
+//   if (err) return handleError(err);
+// });
+
+// //Testing on sample data.
+// //sample data for batch insertion.
+// const sampleDataArray = [
+//   {"business_legal_name":"ABACUS HOSPITALITY LLC","trade_name":"FRESH TASTE CAFE","ownership_type":"LLC - Single Member","naics_code":"722513","naics_description":"Limited-Service Restaurants","license_start_date":"2019-06-01T00:00:00.000","street_address":"700 STEWART ST","city_state_zip":"SEATTLE","state":"WA","zip":"98101","business_phone":"360-553-3087","city_account_number":"0008291010752342","ubi":"603416636"},
+//   {"business_legal_name":"CRAB POT RESTAURANTS INC","trade_name":"THE CRAB POT","ownership_type":"Corporation","naics_code":"722511","naics_description":"Full-Service Restaurants","license_start_date":"2019-01-01T00:00:00.000","street_address":"1301 ALASKAN WAY","city_state_zip":"SEATTLE","state":"WA","zip":"98101","business_phone":"206-623-8600","city_account_number":"0008308990745210","ubi":"6042822600010001"},
+//   {"business_legal_name":"PREMIER MEAT PIES LLC","trade_name":"PREMIER MEAT PIES LLC","ownership_type":"LLC - Multi Member","naics_code":"722513","naics_description":"Limited-Service Restaurants","license_start_date":"2018-11-01T00:00:00.000","street_address":"1001 ALASKAN WAY # 105","city_state_zip":"SEATTLE","state":"WA","zip":"98101","business_phone":"206-619-0499","city_account_number":"0007842320744027","ubi":"603512468"},
+//   {"business_legal_name":"SAI RESTAURANTS ENTERPRISE INC","trade_name":"ZAIKA RESTAURANT AND LOUNGE","ownership_type":"Corporation","naics_code":"722511","naics_description":"Full-Service Restaurants","license_start_date":"2019-09-11T00:00:00.000","street_address":"1100 PIKE ST","city_state_zip":"SEATTLE","state":"WA","zip":"98101","business_phone":"206-499-4949","city_account_number":"0007312120755875","ubi":"6030512120010002"},
+//   {"business_legal_name":"SCHWARTZ BROTHERS RESTAURANTS","trade_name":"DANIELS BROILER DOWNTOWN SEATTLE","ownership_type":"General Partnership","naics_code":"722511","naics_description":"Full-Service Restaurants","license_start_date":"2018-12-01T00:00:00.000","street_address":"808 HOWELL ST #200","city_state_zip":"SEATTLE","state":"WA","zip":"98101","business_phone":"425-455-3948","city_account_number":"0005724290742676","ubi":"6024090030010016"},
+//   {"business_legal_name":"WASHINGTON TAPROOMS LLC","trade_name":"LOCUST CIDER","ownership_type":"LLC - Multi Member","naics_code":"722511","naics_description":"Full-Service Restaurants","license_start_date":"2019-09-12T00:00:00.000","street_address":"1222 POST ALY","city_state_zip":"SEATTLE","state":"WA","zip":"98101","business_phone":"646-783-9267","city_account_number":"0008416520756455","ubi":"6044539810010001"}
+// ];
+//
+// //update the 722513 records to be more than one year old
+// async function makeSomeOld() {
+//   let allRecords = await getSampleOutput(Testrestaurant);
+//   console.log("allRecords:", allRecords);
+//   async function transform(entries) {
+//     entries.forEach(async function(entry) {
+//       if (entry.naics_code === '722513'){
+//         entry.license_start_date.setFullYear(entry.license_start_date.getFullYear() - 1);
+//         await entry.markModified('license_start_date');
+//         await entry.save();
+//       }
+//     })
+//     let changeFiles = await getSampleOutput(Testrestaurant, {naics_code: '722513'});
+//     return changeFiles;
+//     // console.log("MODIFIED FILES:", changeFiles);
+//   }
+//   await transform(allRecords);
+//   let oldRecords = await getSampleOutput(Testrestaurant, {naics_code: '722513'}, "If these have old dates, you win.");
+//   // await removeTestRestaurants();
+//   return oldRecords;
+// }
+//
+// //Write a basic query that returns the contents of testrestaurants
+// async function getSampleOutput(model, query = {}, message = "No message passed") {
+//   console.log("message:", message);
+//   let rawResponse = await model.find(query, function(err, restaurants) {
+//     return restaurants;
+//   });
+//   console.log("response from getSampleOutput", rawResponse);
+//   console.log("Count:", rawResponse.length);
+//   return rawResponse;
+// };
+//
+// //empty testrestaurants
+// async function removeTestRestaurants(){
+//   let response = await Testrestaurant.remove({});
+//   console.log("Records deleted:", response.deletedCount);
+// }
+//
+// //Composite testing function
+// async function runTestCode() {
+//   mongoose.connect('mongodb://localhost/test', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true
+//   });
+//   await insertRestaurant(sampleDataArray, Testrestaurant);
+//   let oldRecords = await makeSomeOld();
+//   await removeTestRestaurants();
+//   mongoose.connect('mongodb://localhost/restaurants', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true
+//   });
+//   return oldRecords;
+// }
+
+//run test code
+// runTestCode()
+// .catch(function(err){
+//   console.log("I'm an error log in runTestCode's catch", err);
+// });
+
+// async function proveAPIWorks(){
+//   let url = geoApi.replace("SEARCH_STRING", "Empire%20State%20Building");
+//   console.log("url:", url);
+//   console.log("this log is in proveAPIWorks - typeof url", typeof url);
+//   let init = {};
+//   let headers = {
+//     "async": true,
+//     "crossDomain": true,
+//     "url": url,
+//     "method": "GET"
+//   }
+//   init.headers = headers
+//   try {
+//     let response = await fetch(url, init);
+//     let output = await response.json();
+//     console.log("this is the output log in proveAPIWorks", output);
+//   } catch(err){
+//     console.log("this log an error in proveAPIWorks", err);
 //   }
 // }
